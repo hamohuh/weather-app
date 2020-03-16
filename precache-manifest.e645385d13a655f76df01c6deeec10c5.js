7aae05ea6455efa38c7e76bc855f04a42668629f
hamohuh <hamodroid@gmail.com>
2020-03-16 20:07:41 +0200

    Updates

---
+++ precache-manifest.e645385d13a655f76df01c6deeec10c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee16c6b772f0158a90971d7d3f54590",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "cc4cae2f85833a79f34a",
+    "url": "/weather-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f0a06a5b1ca5e18869ab",
+    "url": "/weather-app/static/css/main.ff2bfa2b.chunk.css"
+  },
+  {
+    "revision": "cc4cae2f85833a79f34a",
+    "url": "/weather-app/static/js/2.0ebc4131.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/weather-app/static/js/2.0ebc4131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a06a5b1ca5e18869ab",
+    "url": "/weather-app/static/js/main.3e379c2d.chunk.js"
+  },
+  {
+    "revision": "089fb1fee07ab9da1ce8",
+    "url": "/weather-app/static/js/runtime-main.029e69d5.js"
+  },
+  {
+    "revision": "72dc42175cbad7ddd00487dd3c07ac66",
+    "url": "/weather-app/static/media/background.72dc4217.jpg"
+  }
+]);
